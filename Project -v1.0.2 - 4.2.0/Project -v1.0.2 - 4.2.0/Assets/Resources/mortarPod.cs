cb9a74784e4164c186f9b3b468feaf3132e90517
Zuwra <jordanjohnson16@gmail.com>
2015-12-13 10:30:04 -0700

    Created MortarPod

---
+++ Project -v1.0.2 - 4.2.0/Project -v1.0.2 - 4.2.0/Assets/Resources/mortarPod.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mortarPod : MonoBehaviour, Validator, Notify {
+
+
+
+
+	public float totalShots;
+	public float reloadRate;
+	private float shotCount;
+	private IWeapon weapon;
+
+	public bool FireAll;
+
+	private float nextActionTime;
+
+	// Use this for initialization
+	void Start () {
+
+
+		shotCount = totalShots;
+		weapon = this.gameObject.GetComponent<IWeapon> ();
+		nextActionTime = Time.time;
+
+		weapon.validators.Add (this);
+		weapon.triggers.Add (this);
+		if (FireAll) {
+			weapon.attackPeriod = .05f;
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Time.time > nextActionTime) {
+			nextActionTime += reloadRate;
+			if(shotCount < totalShots)
+			{
+				shotCount++;
+			}
+		
+		}
+
+
+	
+	}
+
+	public void toggleFireAll()
+	{FireAll = ! FireAll;
+
+
+	}
+
+
+	public bool validate(GameObject source, GameObject target)
+		{if (shotCount > 0) {
+			return true;
+		
+		}
+		return false;
+	}
+
+	public void trigger(GameObject source, GameObject proj, GameObject target)
+		{
+		shotCount --;
+		if (FireAll) {
+			weapon.attackPeriod = .05f;
+		}
+
+
+	}
+
+}
