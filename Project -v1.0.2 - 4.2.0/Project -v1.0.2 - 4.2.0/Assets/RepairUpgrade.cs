5da4be9db599b50d52f820f687e3ff597d2d95bc
jchip <jordanchip@gmail.com>
2016-03-06 10:56:54 -0700

    Trying to add RepairUpgrade.cs for real now

---
+++ Project -v1.0.2 - 4.2.0/Project -v1.0.2 - 4.2.0/Assets/RepairUpgrade.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RepairUpgrade : Upgrade {
+
+    public int repairAmount = 12;
+
+    override
+    public void applyUpgrade(GameObject obj)
+    {
+
+        UnitManager manager = obj.GetComponent<UnitManager>();
+
+        if (manager)
+        {
+            if (manager.UnitName == "Repair Arm")
+            {
+                obj.GetComponent<RepairTurret>().repairRate = repairAmount;
+            }
+        }
+    }
+}
