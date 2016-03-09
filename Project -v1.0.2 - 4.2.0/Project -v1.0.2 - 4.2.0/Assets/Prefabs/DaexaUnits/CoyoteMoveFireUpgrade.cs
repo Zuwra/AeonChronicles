f8aa0ea7057508e3e275bafdd829906ce2b71c86
jchip <jordanchip@gmail.com>
2016-03-08 19:17:22 -0700

    Added CoyoteMoveFireUpgrade.cs

---
+++ Project -v1.0.2 - 4.2.0/Project -v1.0.2 - 4.2.0/Assets/Prefabs/DaexaUnits/CoyoteMoveFireUpgrade.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoyoteMoveFireUpgrade : Upgrade {
+
+    override
+    public void applyUpgrade(GameObject obj)
+    {
+
+        UnitManager manager = obj.GetComponent<UnitManager>();
+
+        if (manager)
+        {
+            if (manager.UnitName == "Coyote")
+            {
+                obj.GetComponent<StandardInteract>().attackWhileMoving = true;
+            }
+        }
+    }
+}
