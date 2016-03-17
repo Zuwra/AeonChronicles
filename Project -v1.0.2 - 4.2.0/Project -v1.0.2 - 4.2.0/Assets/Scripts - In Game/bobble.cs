de0d2fd11c13cfbc8f7047d67e37484ab2c09a9e
Zuwra <jordanjohnson16@gmail.com>
2016-03-17 16:38:56 -0600

    model poly simplifications

--- Project -v1.0.2 - 4.2.0/Project -v1.0.2 - 4.2.0/Assets/Scripts - In Game/bobble.cs
+++ Project -v1.0.2 - 4.2.0/Project -v1.0.2 - 4.2.0/Assets/Scripts - In Game/bobble.cs
@@ -7,18 +7,49 @@ public class bobble : animate {
 	public float speed;                  //Set in Inspector 
 	private float tempVal;
 	private Vector3 tempPos;
-	
-	void Start () 
-	{tempPos = this.gameObject.transform.position;
-		tempVal = transform.position.y;
+
+	public bool x;
+	public bool z;
+
+
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		tempPos = this.gameObject.transform.position;
+		if (z) {
+			tempVal = transform.position.z;
+		} else if (x) {
+			tempVal = transform.position.x;
+		} else {
+			tempVal = transform.position.y;
+		}
+
 	}
-	
-	void Update () 
-	{        if (active) {
-			tempPos.y = tempVal + amplitude * Mathf.Sin (speed * Time.time);
+
+	// Update is called once per frame
+	void Update () {
+		if (active) {
+			if (z) {
+				tempPos.z = tempVal + amplitude * Mathf.Sin (speed * Time.time);
+			
+			} else if (x) {
+				tempPos.x = tempVal + amplitude * Mathf.Sin (speed * Time.time);
+
+
+			} else {
+				tempPos.y = tempVal + amplitude * Mathf.Sin (speed * Time.time);
+			
+			}
 			transform.position = tempPos;
 		}
 	}
+
+
+
 
 
 
