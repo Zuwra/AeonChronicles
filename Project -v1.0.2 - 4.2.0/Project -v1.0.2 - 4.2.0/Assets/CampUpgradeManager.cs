d60e5430a0a622b5c60a6054061f474f163aa3a4
Zuwra <jordanjohnson16@gmail.com>
2016-04-30 17:37:49 -0600

    New Campaign Mechanics Scene

---
+++ Project -v1.0.2 - 4.2.0/Project -v1.0.2 - 4.2.0/Assets/CampUpgradeManager.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CampUpgradeManager : MonoBehaviour {
+
+	public GameObject TurretMenu;
+	public GameObject vehicleMenu;
+	public GameObject StructureMenu;
+
+	private GameObject currentMenu;
+
+	[System.Serializable]
+	public struct UnitsUnlocked{
+
+		public string name;
+		public bool unlocked;
+		public GameObject UIPart;
+
+	}
+
+	public List<CampaignUpgrade.UpgradesPiece> myUpgrades= new List<CampaignUpgrade.UpgradesPiece>();
+
+	public List<UnitsUnlocked> myUnits= new List<UnitsUnlocked>();
+
+
+	// Use this for initialization
+	void Start () {
+
+		foreach (UnitsUnlocked unit in myUnits) {
+			unit.UIPart.SetActive (unit.unlocked);
+		
+		}
+
+		TurretMenu.SetActive (false);
+		StructureMenu.SetActive (false);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void SetMenu(GameObject obj)
+	{
+		if (currentMenu != null) {
+			currentMenu.SetActive (false);
+		}
+		currentMenu = obj;
+		obj.SetActive (true);
+
+
+	}
+
+
+
+}
---
+++ Project -v1.0.2 - 4.2.0/Project -v1.0.2 - 4.2.0/Assets/CampaignUpgrade.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class CampaignUpgrade : MonoBehaviour {
+
+
+	public Text theDescription;
+	public Dropdown myDropDown;
+	public List<UpgradesPiece> myUpgrades = new List<UpgradesPiece>();
+
+	public Image myPic;
+
+	public enum upgradeType{general, vehicle, tank, structure, turret, munition, coyote, tortoise, OreProcessor, Hornet, 
+	gatling, rail, repair, mortar, construction, bunker, factory}
+
+
+	public List<upgradeType> myTypes = new List<upgradeType> ();
+
+	[System.Serializable]
+	public struct UpgradesPiece{
+
+		public string name;
+		[TextArea(2,10)]
+		public string description;
+		public upgradeType myType;
+		public bool unlocked;
+		public Upgrade pointer;
+		public Sprite pic;
+
+	}
+
+
+	private CampUpgradeManager myManager;
+
+	public void setUpgrade()
+	{
+		if (myUpgrades.Count > 0) {
+			theDescription.text = myUpgrades [myDropDown.value].description;
+	
+			myPic.sprite = myUpgrades [myDropDown.value].pic;
+
+		}
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		myManager = GameObject.FindObjectOfType<CampUpgradeManager> ();
+		List<string> options = new List<string> ();
+		foreach (UpgradesPiece up in myManager.myUpgrades) {
+			if (up.unlocked && myTypes.Contains (up.myType)) {
+				myUpgrades.Add (up);
+				options.Add (up.name);
+			
+			}
+
+		}
+
+		myDropDown.AddOptions (options);
+	
+		setUpgrade ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
