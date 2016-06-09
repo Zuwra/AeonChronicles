d5e3b8f3815c6708c62832ecb859bc0f4f497817
Zuwra <jordanjohnson16@gmail.com>
2016-06-09 12:28:16 -0600

    Mining Saw now has working indicator zones.

--- Project -v1.0.2 - 4.2.0/Project -v1.0.2 - 4.2.0/Assets/MiningSaw.cs
+++ Project -v1.0.2 - 4.2.0/Project -v1.0.2 - 4.2.0/Assets/MiningSaw.cs
@@ -7,68 +7,58 @@ public class MiningSaw : MonoBehaviour {
 
 	public Animator myAnimator;
 	UnitManager myManager;
-	int inAction; //0 = idle, 1 = rotating, 2 = attacking
-	List<GameObject> objects;
+	public float startTime;
 
-	public GameObject chopDecal;
-	public GameObject sliceDecal;
-	public GameObject arm;
-	public float speed;
+
+	public List<GameObject> objects;
+
+
+	public List<float> timers;
+
+
 
 	private Vector3 targetLocation;
-	private int state = 0;
+	private int state = -1;
 	private float nextActionTime;
+	private bool Turnedon;
 
 	// Use this for initialization
 	void Start () {
 		myManager = GetComponent<UnitManager> ();
-		objects = myManager.enemies;
+		//objects = myManager.enemies;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Time.time > nextActionTime) {
-			nextActionTime += 3f;
+		if (Turnedon) {
+			if (Time.time > nextActionTime) {
+				if (state >= 0 && objects [state] != null) {
+					objects [state].SetActive (false);
+				}
+				state++;
+				if (state == objects.Count) {
+					state = 0;
+				}
+				if (objects [state]) {
+					objects [state].SetActive (true);
+				}
+				nextActionTime += timers [state];
 
-			state++;
-			if (state > 5) {
-				state = 1;}
-			Debug.Log ("Setting state " + state);
-			myAnimator.SetInteger ("State", state);
-		}
 
-
-		/*
-		if (inAction == 0) {
-			if (objects.Count > 0) {
-				GameObject target = objects [Random.Range (0, objects.Count)];
-				targetLocation = target.transform.position;
-				chopDecal.transform.position = targetLocation;
-				targetLocation.y = arm.transform.position.y;
-
-				inAction = 1;
+				Debug.Log ("Setting state " + state);
+				//myAnimator.SetInteger ("State", state);
 			}
-		} else if (inAction == 1) {
-			
-			Debug.Log (targetLocation + "   " + arm.transform.position);
-			Vector3 angle = targetLocation - arm.transform.position;
 
-			arm.transform.Rotate (Vector3.up, -30 * Time.deltaTime);
-			Debug.Log (Vector3.Angle (angle, arm.transform.forward));
-			if (Vector3.Angle (angle, arm.transform.forward) - 100 < 5 && Vector3.Angle (angle, arm.transform.forward) - 100 > 0) {
-				inAction = 2;
-				Debug.Log ("At Target");
-			}
-		
-		
-		} else {
-		
 		}
-			*/
 	
 	}
 
-
+	public void turnOn()
+	{
+		Turnedon = true;
+		myAnimator.SetInteger ("State", 1);
+		nextActionTime = Time.time + startTime;
+	}
 
 
 
---
+++ Project -v1.0.2 - 4.2.0/Project -v1.0.2 - 4.2.0/Assets/sawTrigger.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sawTrigger : MonoBehaviour {
+
+
+	public MiningSaw mySaw;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+
+		if (other.GetComponent<UnitManager> ())
+		if (other.GetComponent<UnitManager> ().PlayerOwner == 1) {
+			mySaw.turnOn ();
+			Destroy (this.gameObject);
+		}
+	}
+
+
+
+}
