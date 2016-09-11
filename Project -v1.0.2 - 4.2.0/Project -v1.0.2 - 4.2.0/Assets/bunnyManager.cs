f43f299051d33e35be9aabb6f99eab3ffd0e51d8
Zuwra <jordanjohnson16@gmail.com>
2016-09-10 23:59:11 -0600

    BUNNY ASSAULT MODE!

---
+++ Project -v1.0.2 - 4.2.0/Project -v1.0.2 - 4.2.0/Assets/bunnyManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class bunnyManager : MonoBehaviour {
+
+	public Text bunnyCount;
+	public int currAmount;
+	public int maxAmount;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void changeInBunnyCount(int change)
+	{
+		currAmount += change;
+		bunnyCount.text = "Vicious and cute bunny count \n" + currAmount + " / " + maxAmount;
+
+		if (currAmount >= maxAmount) {
+			VictoryTrigger.instance.Lose();
+		}
+		else if (currAmount <= 0) {
+			VictoryTrigger.instance.Win();
+		}
+	}
+}
---
+++ Project -v1.0.2 - 4.2.0/Project -v1.0.2 - 4.2.0/Assets/bunnyPopulate.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using Pathfinding.RVO;
+
+public class bunnyPopulate : MonoBehaviour, Modifier {
+
+	public float repopulateTime;
+	public float randomSpawnRange;
+	private float nextRepopulate;
+	private UnitStats myStats;
+	// Use this for initialization
+	void Start () {
+		GameObject.FindObjectOfType<bunnyManager> ().changeInBunnyCount (1);
+		nextRepopulate = Time.time + repopulateTime + Random.Range(0,randomSpawnRange);
+		GetComponent<UnitStats> ().addDeathTrigger (this);
+		myStats = GetComponent<UnitStats> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Time.time > nextRepopulate) {
+
+			Vector3 hitzone = this.transform.position;
+			float radius = Random.Range (4, 15);
+			float angle = Random.Range (0, 360);
+
+			hitzone.x += Mathf.Sin (Mathf.Deg2Rad * angle) * radius;
+			hitzone.z += Mathf.Cos (Mathf.Deg2Rad * angle) * radius;
+
+
+			Instantiate (this.gameObject, hitzone, Quaternion.identity);//do the thing
+			nextRepopulate = Time.time + repopulateTime + Random.Range(0,randomSpawnRange);
+			repopulateTime += 3;
+
+			myStats.Maxhealth += 5;
+			myStats.heal (5);
+			this.gameObject.transform.localScale = this.gameObject.transform.localScale + Vector3.one *.1f;
+			GetComponent<RVOController> ().radius += .1f;
+		}
+	
+	}
+
+
+	public float modify (float a,GameObject deathSource){
+		GameObject.FindObjectOfType<bunnyManager> ().changeInBunnyCount (-1);
+		return a;
+	}
+}
