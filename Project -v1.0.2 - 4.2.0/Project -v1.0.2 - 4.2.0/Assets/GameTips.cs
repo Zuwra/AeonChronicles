ff4947c2ee96ba62934a54e61330e27768eb80c8
Zuwra <jordanjohnson16@gmail.com>
2016-12-13 00:20:09 -0700

    bug fixes, new ui art

--- Project -v1.0.2 - 4.2.0/Project -v1.0.2 - 4.2.0/Assets/GameTips.cs
+++ Project -v1.0.2 - 4.2.0/Project -v1.0.2 - 4.2.0/Assets/GameTips.cs
@@ -6,14 +6,15 @@ using System.Collections.Generic;
 public class GameTips : MonoBehaviour {
 
 	public Text myText;
-	public List<string> myTips = new List<string> ();
+
+	public TextHolder textHold;
 	private int currentIndex;
 
 
 	// Use this for initialization
 	void Start () {
-		currentIndex = Random.Range (0, myTips.Count);
-		myText.text = myTips [currentIndex];
+		currentIndex = Random.Range (0, textHold.myTexts [0].myTexts.Count);
+		myText.text =  textHold.myTexts [0].myTexts [currentIndex];
 	
 	}
 	
@@ -27,19 +28,19 @@ public class GameTips : MonoBehaviour {
 	public void nextTip()
 	{
 		currentIndex++;
-		if (currentIndex == myTips.Count) {
+		if (currentIndex == textHold.myTexts [0].myTexts.Count) {
 			currentIndex = 0;
 		}
-		myText.text = myTips [currentIndex];
+		myText.text = textHold.myTexts [0].myTexts [currentIndex];
 	}
 
 	public void prevTip()
 	{
 		currentIndex++;
 		if (currentIndex ==-1) {
-			currentIndex = myTips.Count -1;
+			currentIndex = textHold.myTexts [0].myTexts.Count -1;
 		}
-		myText.text = myTips [currentIndex];
+		myText.text = textHold.myTexts [0].myTexts [currentIndex];
 
 	}
 
