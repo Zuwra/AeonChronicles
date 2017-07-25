a4eb0a64b776577cb03ab068e78da12f38b839b9
matthew57 <nosnhojdm@gmail.com>
2017-07-25 09:57:22 -0700

    New Money Level!

---
+++ Project -v1.0.2 - 4.2.0/Project -v1.0.2 - 4.2.0/Assets/moneyObjective.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class moneyObjective : Objective {
+
+	public Slider moneySlide;
+	public RaceManager myRace;
+	public Text myText;
+
+	public float moneyVictory;
+	// Use this for initialization
+	void Start () {
+		base.Start ();
+		InvokeRepeating ("UpdateMoney", .75f, .7f);
+	}
+	
+	// Update is called once per frame
+	void UpdateMoney () {
+
+		myText.text = myRace.ResourceOne +"/"+ moneyVictory;
+		moneySlide.value = myRace.ResourceOne / moneyVictory;
+		if (myRace.ResourceOne >= moneyVictory) {
+			complete ();
+		}
+
+	}
+}
