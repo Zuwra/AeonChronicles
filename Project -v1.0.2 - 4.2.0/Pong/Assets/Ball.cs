43db4703ec7a4b9eca3cda9faf64af56118a199f
matthew57 <nosnhojdm@gmail.com>
2017-08-11 00:52:42 -0700

    New Levels!

---
+++ Project -v1.0.2 - 4.2.0/Pong/Assets/Ball.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+	
+
+	Vector3 movement = new Vector3(300,300,0);
+
+	// Update is called once per frame
+	void Update () {
+
+		transform.position += movement * Time.deltaTime;
+		if (transform.position.x < 0) {
+			movement.x *= (-1);
+		}
+
+		if (transform.position.x > Screen.width) {
+			movement.x *= (-1);
+		}
+
+		if (transform.position.y > Screen.height) {
+			movement.y *= (-1);
+		}
+
+		if (transform.position.y < 0) {
+			movement.y *= (-1);
+		}
+	}
+}
---
+++ Project -v1.0.2 - 4.2.0/Pong/Assets/Paddle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.W)) {
+			transform.position = transform.position +  new Vector3(0,250,0) * Time.deltaTime;
+		}
+
+		if (Input.GetKey (KeyCode.S)) {
+			transform.position = transform.position -  new Vector3(0,250,0) * Time.deltaTime;
+		}
+		
+	}
+}
