47f84d2fe798a1f956d029ab0e77473b47d1544f
matthew57 <nosnhojdm@gmail.com>
2017-08-12 00:02:02 -0700

    HeritageofNull

---
+++ Project -v1.0.2 - 4.2.0/Project -v1.0.2 - 4.2.0/Assets/ProxySpawner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProxySpawner : MonoBehaviour {
+
+	public int maxSpawnCount;
+	public int cooldown;
+	[Tooltip("This is multiplied by the difficulty number")]
+	public int waveCount;
+
+
+}
