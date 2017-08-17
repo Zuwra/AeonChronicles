8c924a5c2c478f77d719b55530f28d4d26854d5f
matthew57 <nosnhojdm@gmail.com>
2017-08-16 23:25:56 -0700

    Gigapede!

---
+++ Project -v1.0.2 - 4.2.0/Project -v1.0.2 - 4.2.0/Assets/Gigapede.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gigapede : MonoBehaviour, Modifier {
+
+	public List<GameObject> segments = new List<GameObject> ();
+
+	public GameObject toSpawn;
+	int segmentLeft = 5;
+	float DamageAmount;
+	private UnitStats mystats;
+
+	// Use this for initialization
+	void Start () {
+
+		mystats = GetComponent<UnitStats> ();
+		mystats.addModifier (this);
+		DamageAmount = mystats.Maxhealth / segmentLeft;
+	}
+
+
+
+	public float modify(float damage, GameObject source, DamageTypes.DamageType theType)
+	{
+
+		if (segmentLeft > 1 && (mystats.health - (damage -mystats.armor)) < (segmentLeft - 1) * DamageAmount) {
+			segmentLeft--;
+		
+			mystats.armor--;
+			Instantiate (toSpawn, segments[segmentLeft].transform.position, Quaternion.identity);
+			segments [segmentLeft].SetActive (false);
+		}
+
+
+		return damage;
+	}
+}
